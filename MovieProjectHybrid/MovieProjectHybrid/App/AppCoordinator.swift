60cb39119e94176e32beac9991e8bf895e75a5bf
cagatayegilmez <cagatayegilmez08@gmail.com>
2026-01-19 19:30:21 +0300

    [hybrid-project] Add coordinators

---
+++ MovieProjectHybrid/MovieProjectHybrid/App/AppCoordinator.swift
@@ -0,0 +1,100 @@
+//
+//  AppCoordinator.swift
+//  MovieProjectHybrid
+//
+//  Created by Çağatay Eğilmez on 19.01.2026.
+//
+
+import UIKit
+
+protocol AppScreenFactory {
+
+    func makeMovieList() -> UIViewController
+    func makeMovieDetail(movieId: Int) -> UIViewController
+}
+
+final class AppCoordinator: NSObject, NavigationCoordinator {
+    var childCoordinators: [Coordinator] = []
+
+    let navigationController: UINavigationController
+    private let factory: AppScreenFactory
+    private let navBarStyle: (AppRoute) -> NavigationBarStyle
+
+    init(
+        navigationController: UINavigationController = UINavigationController(),
+        factory: AppScreenFactory,
+        navBarStyle: @escaping (AppRoute) -> NavigationBarStyle = { _ in .visible }
+    ) {
+        self.navigationController = navigationController
+        self.factory = factory
+        self.navBarStyle = navBarStyle
+        super.init()
+        self.navigationController.delegate = self
+    }
+
+    func start() {
+        show(route: .movieList, animated: false)
+    }
+
+    func show(route: AppRoute, animated: Bool = true) {
+        let vc = makeViewController(for: route)
+        applyNavigationBarStyle(for: route, animated: animated)
+
+        switch route {
+        case .movieList:
+            navigationController.setViewControllers([vc], animated: animated)
+        case .movieDetail:
+            navigationController.pushViewController(vc, animated: animated)
+        }
+    }
+
+    // MARK: - Private
+
+    private func makeViewController(for route: AppRoute) -> UIViewController {
+        switch route {
+        case .movieList:
+            return factory.makeMovieList().tagged(route)
+        case .movieDetail(let movieId):
+            return factory.makeMovieDetail(movieId: movieId).tagged(route)
+        }
+    }
+
+    private func applyNavigationBarStyle(for route: AppRoute, animated: Bool) {
+        let hidden: Bool
+        switch navBarStyle(route) {
+        case .visible: hidden = false
+        case .hidden:  hidden = true
+        }
+        navigationController.setNavigationBarHidden(hidden, animated: animated)
+    }
+}
+
+// MARK: - UINavigationControllerDelegate
+
+extension AppCoordinator: UINavigationControllerDelegate {
+    func navigationController(
+        _ navigationController: UINavigationController,
+        willShow viewController: UIViewController,
+        animated: Bool
+    ) {
+        guard let route = viewController.appRoute else { return }
+        applyNavigationBarStyle(for: route, animated: animated)
+    }
+}
+
+// MARK: - Route tagging (same file, once)
+
+private var appRouteKey: UInt8 = 0
+
+private extension UIViewController {
+    var appRoute: AppRoute? {
+        get { objc_getAssociatedObject(self, &appRouteKey) as? AppRoute }
+        set { objc_setAssociatedObject(self, &appRouteKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
+    }
+
+    /// fluent: vc.tagged(.movieList)
+    func tagged(_ route: AppRoute) -> UIViewController {
+        self.appRoute = route
+        return self
+    }
+}
---
+++ MovieProjectHybrid/MovieProjectHybrid/App/BaseCoordinator.swift
@@ -0,0 +1,40 @@
+//
+//  BaseCoordinator.swift
+//  MovieProjectHybrid
+//
+//  Created by Çağatay Eğilmez on 19.01.2026.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+
+    /// An array of child coordinators
+    var childCoordinators: [Coordinator] { get set }
+    /// Function of coordinator start
+    func start()
+}
+
+protocol NavigationCoordinator: Coordinator {
+
+    /// UIKit navigation element
+    var navigationController: UINavigationController { get }
+}
+
+enum AppRoute: Equatable {
+
+    /// Main screen, movie list.
+    case movieList
+    /// Movie detail screen
+    ///
+    ///  - Parameter movieId: Int id of movie for fetching details
+    case movieDetail(movieId: Int)
+}
+
+enum NavigationBarStyle: Equatable {
+
+    /// Visible navigation bar style
+    case visible
+    /// Invisible navigation bar style
+    case hidden
+}
